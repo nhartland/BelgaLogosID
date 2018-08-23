88798519f9f9998887ab66b7758a2fc1dd5ba250
Nathan Hartland <n.p.hartland@vu.nl>
2018-08-23 14:22:07 +0200

    Captions for multi_table plotting

--- util.py
+++ util.py
@@ -3,9 +3,20 @@ from IPython.display import HTML
 
 # Formatting for tables
 # Original source: Eric Moyer (https://github.com/epmoyer/ipy_table/issues/24)
-def multi_table(table_list):
-    ''' Accepts a list of IpyTable objects and returns a table which contains each IpyTable in a cell
+# Modified by nh to include captions
+def multi_table(table_list, captions=None):
+    ''' Accepts a list of pandas dataframes and returns a table which contains
+    each dataframe as a table in a cell. An optional captions argument
+    annotates each dataframe before rendering them to a table.
     '''
+    if captions != None:
+        if len(captions) != len(table_list):
+            raise ValueError("multi_table requires a caption list as long as the table list")
+        captioned_tables = []
+        for i in range(len(captions)):
+            captioned_tables.append((table_list[i].style.set_caption(captions[i])))
+        table_list = captioned_tables
+
     return HTML(
         '<table><tr style="background-color:white;">' +
         ''.join(['<td>' + table._repr_html_() + '</td>' for table in table_list]) +
