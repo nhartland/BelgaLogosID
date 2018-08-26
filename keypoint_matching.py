02d1c636abdee72402bbcc378bd5924b1430ee34
Nathan Hartland <n.p.hartland@vu.nl>
2018-08-26 15:56:53 +0200

    Handle cases of empty clusters

--- keypoint_matching.py
+++ keypoint_matching.py
@@ -117,10 +117,20 @@ def get_matching_boundingbox(template, test, matcher, MIN_MATCHES, MIN_INLIERS):
         Returns:
             A list of vertices specifying the bounding-box of the match
     """
+    # Number of provided descriptors
+    n_template = len(template.descriptors)
+    n_test     = len(test.descriptors)
+
+    # Sanity checks
+    if n_template < MIN_INLIERS or n_template < MIN_MATCHES:
+        return None
+    if n_test < MIN_INLIERS or n_test < MIN_MATCHES:
+        return None
+
     # Perform match
     matches = matcher.match(template.descriptors, test.descriptors)
     if __DEBUG__ is True:
-        print(len(matches) / min(len(template.keypoints), len(test.keypoints)), " matches found")
+        print(len(matches) / min(n_template, n_test), " matches found")
 
     # If there are sufficient matches, attempt to build a homography
     if len(matches) >= MIN_MATCHES:
@@ -154,6 +164,7 @@ def bruteforce_match_clusters(template_img, test_img, finder, norm,
     template = KeypointSet(template_img, template_keypoints, template_descriptors)
 
     # Compute keypoint and descriptor clusters for target image
+    print(len(template.descriptors), len(test.descriptors))
     kp_clusters, ds_clusters = meanshift_keypoint_clusters(test_img, finder, quantile=QUANTILE)
 
     # Set up brute-force matcher
